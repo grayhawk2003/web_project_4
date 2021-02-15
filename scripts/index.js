ca8c37a2fe39e7154bf14bd5b9aecc698f9f4bbb
Meg Younger <megan.g.younger@gmail.com>
2021-02-15 16:39:19 -0500

    code javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,22 @@
 let button = document.querySelector('.profile__edit-button');
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
-let close = document.querySelector('.modal__close-icon');
+let hide = document.querySelector('.modal__close-button');
 let submit = document.querySelector('.modal__form-submit');
 
 button.addEventListener('click', function() {
     modal.style.display = 'block';
     overlay.style.display = 'block';
-    close.style.display = 'block';
+    hide.style.display = 'block';
 });
 
-close.addEventListener('click', function() {
+hide.addEventListener('click', function() {
     modal.style.display = 'none';
-    close.style.display = 'none';
+    hide.style.display = 'none';
+    overlay.style.display = 'none';
 });
 
 submit.addEventListener('click', function() {
-    
+
 })
 
