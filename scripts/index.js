e26e36f4fad842cea926fd527de6e85e2f52bb73
Meg Younger <megan.g.younger@gmail.com>
2021-02-15 12:25:24 -0500

    code javascript and modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,16 @@
 let button = document.querySelector('.profile__edit-button');
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
+let close = document.querySelector('.modal__close-icon');
 
 button.addEventListener('click', function() {
     modal.style.display = 'block';
     overlay.style.display = 'block';
+    close.style.display = 'block';
 });
+
+close.addEventListener('click', function() {
+    modal.style.display = 'none';
+    close.style.display = 'none';
+});
+
