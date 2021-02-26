9024653304759a18c7bb3cc82f3b561721a1eb33
Meg Younger <megan.g.younger@gmail.com>
2021-02-25 19:25:31 -0500

    debug modal and code javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,35 @@
 let editButton = document.querySelector('.profile__edit-button');
 let modal = document.querySelector('.modal');
 let closeButton = document.querySelector('.modal__close-button');
-let submitButton = document.querySelector('.modal__form-submit');
 let profileName = document.querySelector('.profile__title');
 let profileOccupation = document.querySelector('.profile__occupation');
 let formName = document.querySelector('.modal__form-name');
 let formOccupation = document.querySelector('.modal__form-occupation');
-let form = document.querySelector('.modal__form');
+let editModalForm = document.querySelector('.modal__form');
 
 function closeModal () {
-    modal.style.display = 'none';
-    closeButton.style.display = 'none';
+    modal.classList.remove('modal_open');   
 }
 
 editButton.addEventListener('click', function(evt) {
-    evt.preventDefault(); /*Kevin had me add this*/
-    modal.style.display = 'block';
-    formName.textContent = profileName.value;
-    formOccupation.textContent = profileOccupation.value;
-    /*closeButton.style.display = 'block'; */ 
+    modal.classList.add('modal_open');
+    formName.value = profileName.textContent;
+    formOccupation.value = profileOccupation.textContent;
+    
 });
 
 closeButton.addEventListener('click', function() {
-    modal.style.display = 'none';
-    /*closeButton.style.display = 'none';*/
+    modal.classList.remove('modal_open');
 });
 
-submitButton.addEventListener('click', function(evt) {
-    evt.preventDefault(); /*Kevin had me add this*/
+editModalForm.addEventListener('submit', function(evt) {
+    evt.preventDefault(); 
     profileName.textContent = formName.value;
     profileOccupation.textContent = formOccupation.value;
     closeModal ();
 });    
 
-form.addEventListener('submit', )
+
 
 
 
