e01881994d2b8a0a18fe16bd8952c9fb7a8b3898
Meg Younger <megan.g.younger@gmail.com>
2021-02-27 10:47:05 -0500

    ready for code review

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ let modal = document.querySelector('.modal');
 let closeButton = document.querySelector('.modal__close-button');
 let profileName = document.querySelector('.profile__title');
 let profileOccupation = document.querySelector('.profile__occupation');
-let formName = document.querySelector('.modal__form-name');
-let formOccupation = document.querySelector('.modal__form-occupation');
+let formName = document.querySelector('.modal__form_input_name');
+let formOccupation = document.querySelector('.modal__form_input_occupation');
 let editModalForm = document.querySelector('.modal__form');
 
 function closeModal () {
