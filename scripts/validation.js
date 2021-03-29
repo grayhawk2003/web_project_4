9a84d69b3f02b4c0022f54c16e644267b8843833
Meg Younger <megan.g.younger@gmail.com>
2021-03-29 19:03:39 -0400

    code javascript for form validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,30 +1,61 @@
 /////DECLARATIONS//////
 const formElement = document.querySelector(".modal__form");
+/*const formInputList = Array.from(document.querySelectorAll(".modal__input"));
+const formList = Array.from(document.querySelectorAll(".modal__form"));*/
 const formInput = formElement.querySelector(".modal__input");
-const formError = formElement.querySelector('.${formInput.id}-error');
+const formError = formElement.querySelector(".modal__input-error");
 
 
 ///3 FUNCTIONS TO DEFINE INPUT FIELD'S BEHAVIOR//////
 
 ///1.  SHOW ERROR ELEMENT IN ORDER TO NOTIFY USER////
-const showInputError = (element) => {
-    element.classList.add("modal__input_type_error");
-};
+/*const showInputError = (element, errorMessage) => {
+  formElements.forEach((formElement) => {
+
+  
+    element.classList.add('modal__input_type_error');
+    formError.textContent = errorMessage;
+    formError.classList.add('modal__input-error_active');
+}) 
+};*/
+
+const showInputError = (element, errorMessage) => {  ///ONLY WORKS FOR FIRST FIELD////
+      element.classList.add('modal__input_type_error');
+      formError.textContent = errorMessage;
+      formError.classList.add('modal__input-error_active');
+  
+  };
+
 
 ///2.  HIDE ERROR ELEMEMENT///
 const hideInputError = (element) => {
-    element.classList.remove("modal__input_type_error");
+    element.classList.remove('modal__input_type_error');
+    formError.classList.remove('modal__input-error_active');
+    formError.reset();
+    /*formError.textContent = "";*/
 };
 
 ///3.  CHECK IF FIELD IS VALID, AND ALSO CALLS EITHER SHOWINPUTERROR() OR HIDEINPUTERROR()//////
-const isValid = (element) => {
-  if (!formInput.validity.valid) {
-      showInputError(formInput);
+const isValid = () => {
+    if (!formInput.validity.valid) {
+        showInputError(formInput, formInput.validationMessage);
+    }
+    else {
+        hideInputError(formInput);
+    }
+  };
+
+/*const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
   }
   else {
-      hideInputError(formInput);
+      hideInputError(formElement, inputElement);
   }
-};
+};*/
+
+
+
 
 //EVENT LISTENERS///
 
@@ -36,9 +67,11 @@ formInput.addEventListener("input", isValid);
 
 
 
+
+
 //////////////////////////// LIZA LIVE CODE ///////////////////////////////////////
-/*
-function checkInputValidity(input, form, rest) {
+
+/*function checkInputValidity(input, form, rest) {
   if(input.validity.valid) {
       //hideErrorMessage()
   } else {
@@ -78,4 +111,4 @@ enableValidation({
     inactiveButtonClass: "modal__button_disabled",
     inputErrorClass: "modal__input_type_error",
     errorClass: "modal__error_visible"
-  }); 
+  }); */
